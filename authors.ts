4f5ad4b93f0129bdb2e5881bc4c2094efdb6c75d
tommerty <86270372+tommerty@users.noreply.github.com>
2023-11-04 02:16:28 +0000

    work on contributor article

--- authors.ts
+++ authors.ts
@@ -1,7 +1,7 @@
 export const authors = [
   {
     name: 'Tommerty',
-    image: 'https://avatars.githubusercontent.com/u/10047099?v=4',
+    image: 'https://avatars.githubusercontent.com/u/86270372',
     dorasURL: 'https://doras.to/tommerty',
   },
   {
